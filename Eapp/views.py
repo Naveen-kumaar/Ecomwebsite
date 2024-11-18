2e82168a1a0b7912a7d3964d0233eb3d3521f548
Naveen-kumaar <navinsanjay2001@gmail.com>
2024-11-18 17:06:43 +0530

    creating project

--- Eapp/views.py
+++ Eapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def demo(request):
+    return render(request,'index.html')
+
+def detail(request):
+    return render(request,'detail.html')
--- Ecomwebsite/urls.py
+++ Ecomwebsite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Eapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('demo/',views.demo ,name='demo'),
+    path('detail/',views.detail,name='detail')
 ]
